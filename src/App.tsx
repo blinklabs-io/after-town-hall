f07c7c09695e0e7c269e47f0931d0a7db7bc7709
Brian Lee <bnlee419@gmail.com>
2024-10-13 15:33:19 -0700

    fix: added canonical tag, and resized link to townhall
    Signed-off-by: Brian Lee <bnlee419@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,32 @@ import Final from './Final';
 import LandingPage from './LandingPage';
 import Rooms from './Rooms';
 import Logo from './assets/grayscale_transparent_nobuffer.png';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
-    <div className="min-h-screen font-custom ">
-      <NavBar />
-      <div className="gradient-background">
-        <div className="h-screen flex flex-col ">
-          <div className="flex-grow">
-            <LandingPage />
+    <div>
+      <Helmet>
+        <link rel="canonical" href="https://ath.blinklabs.io/" />
+      </Helmet>
+      <div className="min-h-screen font-custom ">
+        <NavBar />
+        <div className="gradient-background">
+          <div className="h-screen flex flex-col ">
+            <div className="flex-grow">
+              <LandingPage />
+            </div>
           </div>
         </div>
-      </div>
-      <Rooms />
-      <div className="flex items-center justify-center w-full mt-10">
-        <div className="w-full h-px bg-gray-400 flex-grow" />
-        <img className="px-4 text-gray-500 w-64" src={Logo}></img>
-        <div className="w-full h-px bg-gray-400 flex-grow" />
-      </div>
+        <Rooms />
+        <div className="flex items-center justify-center w-full mt-10">
+          <div className="w-full h-px bg-gray-400 flex-grow" />
+          <img className="px-4 text-gray-500 w-64" src={Logo}></img>
+          <div className="w-full h-px bg-gray-400 flex-grow" />
+        </div>
 
-      <Final />
+        <Final />
+      </div>
     </div>
   );
 }
