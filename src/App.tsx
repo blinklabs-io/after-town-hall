b8b7e4d4f13b8f796820deb15176b7ae7fbeb5cf
JaeBrian <bnlee419@gmail.com>
2025-01-03 13:17:39 -0800

    fix: React 19 Compatibility
    Signed-off-by: JaeBrian <bnlee419@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,19 @@ import Final from './Final';
 import LandingPage from './LandingPage';
 import Rooms from './Rooms';
 import Logo from './assets/grayscale_transparent_nobuffer.png';
-import { Helmet } from 'react-helmet';
 
 function App() {
   return (
-    <div>
-      <Helmet>
-        <link rel="canonical" href="https://ath.blinklabs.io/" />
-      </Helmet>
-      <div className="min-h-screen font-custom ">
+    <>
+      <title>ATH | BlinkLabs</title>
+      <meta name="description" content="Join Project Catalyst's After Townhall discussions on blockchain innovation and Cardano ecosystem."
+      />
+      <link rel="canonical" href="https://ath.blinklabs.io/" />
+
+      <div className="min-h-screen font-custom">
         <NavBar />
         <div className="gradient-background">
-          <div className="h-screen flex flex-col ">
+          <div className="h-screen flex flex-col">
             <div className="flex-grow">
               <LandingPage />
             </div>
@@ -24,13 +25,12 @@ function App() {
         <Rooms />
         <div className="flex items-center justify-center w-full mt-10">
           <div className="w-full h-px bg-gray-400 flex-grow" />
-          <img className="px-4 text-gray-500 w-64" src={Logo}></img>
+          <img className="px-4 text-gray-500 w-64" src={Logo} alt="Logo" />
           <div className="w-full h-px bg-gray-400 flex-grow" />
         </div>
-
         <Final />
       </div>
-    </div>
+    </>
   );
 }
 
