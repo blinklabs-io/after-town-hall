7e8fdece316c669ae3144c73adf9fa4486f8c458
musikc (Christina) <136919976+musik-c@users.noreply.github.com>
2025-09-23 15:53:20 -0400

    docs: update 9/24 breakout rm (#266)
    Signed-off-by: musikc (Christina) <136919976+musik-c@users.noreply.github.com>

--- src/afterTownHallData.ts
+++ src/afterTownHallData.ts
@@ -67,6 +67,21 @@ const data = {
   ],
   breakoutRooms: [
     {
+      name: 'Reafino - Catalyst proposals',
+      host: 'Reafino',
+      description: 'Explore Reafino, a RealFi platform on Cardano. Learn about our Catalyst proposals for SmartStarter audit+mainnet launch, SmartPlay on chain games, and open sourcing contracts for the developer ecosystem.',
+    },
+    {
+      name: 'How Cardano can benefit from predictions markets',
+      host: 'John Maylam',
+      description: 'Explore how prediction markets, gaining global attention post-Polymarket, can thrive on Cardano. Join our Playrz breakout for insights and interactive discussions on blockchain gamings future.',
+    },
+        {
+      name: 'Unblocking Builders',
+      host: 'Peter Wolcott',
+      description: 'Relationships in Milestone Based Funding',
+    },
+    {
       name: 'Townhall Check-In will return!',
       host: 'Lidonation',
       description:
@@ -90,7 +105,7 @@ const data = {
   },
   generalInfo: {
     date: 'September 2025',
-    upcomingEvent: 'Fund14 Voting begins start on the 22nd! Join us for Townhalls to learn more on Wednesdays at 3PM UTC',
+    upcomingEvent: 'Fund14 Voting started on the 22nd until Oct 6th! Join us for Townhalls to learn more on Wednesdays at 3PM UTC',
   },
 };
 
